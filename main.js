4c12b6d644f97ee7b17f6364dfeea57609ed2ecc
Sabrina <sabrinacunsolo85@gmail.com>
2020-12-02 18:21:47 +0100

    navbar vue
    stampati in pagina menu navbar e logo

---
+++ main.js
@@ -0,0 +1,12 @@
+var app = new Vue ({
+    el: '#container',
+    data: {
+        // header
+        logoNavbar: 'images/gym_logo_1x.png',
+        menu: ['home', 'about us', 'facilities', 'membership', 'testimonials', 'blog'],
+    },
+    methods: {
+
+    }
+
+});
