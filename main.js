be6262d85751d279c470f6744eadbca96ee2fa53
Sabrina <sabrinacunsolo85@gmail.com>
2020-12-03 12:01:36 +0100

    footer vue

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ var app = new Vue ({
         // header
         logoNavbar: 'images/gym_logo_1x.png',
         menu: ['home', 'about us', 'facilities', 'membership', 'testimonials', 'blog'],
+        // footer
+        parteFooterBottom: ['© Copyright 2012 - 2020', '|', 'Avada Theme by Theme Fusion', '|', 'All Rights Reserved', '|', 'Powered by WordPress'],
+        socialIcon: ['fa-facebook-f', 'fa-twitter', 'fa-instagram'],
     },
     methods: {
 
